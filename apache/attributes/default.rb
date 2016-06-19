783f2905dc3f72a0f4bf5f68ec7c4c13c6eb21ec
prasadgoundla <pgoud0962@gmail.com>
2016-06-19 17:37:09 +0530

    first commit

---
+++ apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"] ["sites"] ["anchoorivarun3"] = { "port" => 80, "domain" => "anchoorivarun3.mylabserver.com" }
+default["apache"] ["sites"] ["anchoorivarun3b"] = {"port" => 80, "domain" => "anchoorivarun3b.mylabserver.com"}
